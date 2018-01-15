a253c8697934a8cb3d6e40c5af5c5cd1b2ec5b92
Ben Lee <ben@Bens-MacBook-Pro.local>
2018-01-15 11:18:59 -0500

    First Commit

---
+++ tippy/tippy/SettingsViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SettingsViewController.swift
+//  tippy
+//
+//  Created by Ben Lee on 1/13/18.
+//  Copyright © 2018 Ben Lee. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tip1Field: UITextField!
+    @IBOutlet weak var tip2Field: UITextField!
+    @IBOutlet weak var tip3Field: UITextField!
+    
+    @IBAction func ChangeDefaults(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        let tip1Int = Int(tip1Field.text!) ?? 15
+        let tip2Int = Int(tip2Field.text!) ?? 20
+        let tip3Int = Int(tip3Field.text!) ?? 30
+
+        defaults.set(tip1Int, forKey: "tip1")
+        defaults.set(tip2Int, forKey: "tip2")
+        defaults.set(tip3Int, forKey: "tip3")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let defaults = UserDefaults.standard
+        let tip1: Int = defaults.integer(forKey: "tip1")
+        let tip2: Int = defaults.integer(forKey: "tip2")
+        let tip3: Int = defaults.integer(forKey: "tip3")
+        
+        tip1Field.text = String(format: "%d", tip1)
+        tip2Field.text = String(format: "%d", tip2)
+        tip3Field.text = String(format: "%d", tip3)
+
+        
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.tintAdjustmentMode = .normal
+        self.navigationController?.navigationBar.tintAdjustmentMode = .automatic
+        print("view will appear")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tippy/tippy/ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Ben Lee on 1/11/18.
+//  Copyright © 2018 Ben Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Tip Calculator"
+        let defaults = UserDefaults.standard
+        defaults.set(15, forKey: "tip1")
+        defaults.set(20, forKey: "tip2")
+        defaults.set(30, forKey: "tip3")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        
+        let tip1: Double = defaults.double(forKey: "tip1")
+        let tip2: Double = defaults.double(forKey: "tip2")
+        let tip3: Double = defaults.double(forKey: "tip3")
+
+        let tip1Double = tip1/100
+        let tip2Double = tip2/100
+        let tip3Double = tip3/100
+
+        let tipPercentages = [tip1Double,tip2Double,tip3Double]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        print(tipControl.selectedSegmentIndex)
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        let defaults = UserDefaults.standard
+        let tip1: Int = defaults.integer(forKey: "tip1")
+        let tip2: Int = defaults.integer(forKey: "tip2")
+        let tip3: Int = defaults.integer(forKey: "tip3")
+        
+        let tip1Text: String = String(format: "%d%%", tip1)
+        let tip2Text: String = String(format: "%d%%", tip2)
+        let tip3Text: String = String(format: "%d%%", tip3)
+
+        
+        tipControl.setTitle(tip1Text, forSegmentAt:0)
+        tipControl.setTitle(tip2Text, forSegmentAt:1)
+        tipControl.setTitle(tip3Text, forSegmentAt:2)
+
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.tintAdjustmentMode = .normal
+        self.navigationController?.navigationBar.tintAdjustmentMode = .automatic
+        print("view will appear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+}
+
